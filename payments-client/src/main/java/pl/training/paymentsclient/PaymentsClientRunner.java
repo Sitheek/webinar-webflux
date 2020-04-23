98dff2d90f06f630659d90c7b3e74d6651a877d5
landrzejewski <landrzejewski.poczta@gmail.com>
2020-04-23 22:03:21 +0200

    final

---
+++ payments-client/src/main/java/pl/training/paymentsclient/PaymentsClientRunner.java
@@ -0,0 +1,39 @@
+package pl.training.paymentsclient;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+@ConfigurationProperties("payments")
+@Setter
+@Component
+@RequiredArgsConstructor
+public class PaymentsClientRunner implements CommandLineRunner {
+
+    private String url;
+    private String path;
+    private final PaymentsRepository paymentsRepository;
+
+    private Flux<Payment> getPayments() {
+        return WebClient.builder()
+                .baseUrl(url)
+                .build()
+                .get()
+                .uri(path)
+                .retrieve()
+                .bodyToFlux(Payment.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        getPayments()
+                .flatMap(paymentsRepository::save)
+                .subscribe(System.out::println);
+    }
+
+}
